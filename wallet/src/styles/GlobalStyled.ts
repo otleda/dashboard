cee88b8e97d02d5103ef480ff0d59fde1dd23024
Adelto Oliveira Almeida <adtguitar@gmail.com>
2020-11-27 16:33:02 -0300

    Up Diretory

--- wallet/src/styles/GlobalStyled.ts
+++ wallet/src/styles/GlobalStyled.ts
@@ -1,17 +1,25 @@
-import {createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle `
 
     * {
-        margin: 0;
+        margin: 0; 
         padding: 0;
         box-sizing: border-box;
     }
 
     html, body, #root {
         height: 100%;
-        background: #00ffc3;
-    }  
+        font-family: 'Roboto', sans-serif;
+         
+    } 
 
-    
+    input, button {
+        border: none;
+        outline: none;
+    } 
+
+    button {
+        cursor: pointer;
+    }
 `;

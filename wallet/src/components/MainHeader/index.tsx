2d17ee120beb2ea02a8dc0189bd11f97157967ab
Adelto Oliveira Almeida <adtguitar@gmail.com>
2020-11-30 19:46:07 -0300

    Refatorando o codigo

--- wallet/src/components/MainHeader/index.tsx
+++ wallet/src/components/MainHeader/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import emojis from  '../../utilities/emojis';
+
 import { Container, Profile, Welcome, UserName } from './styles';
 
 const MainHeader: React.FC = () => {
+
+    const emoji = useMemo(()=>{
+        const indice = Math.floor(Math.random() * emojis.length);
+        return emojis[indice];
+    },[]);
+
     return(
         <Container>
             toggle
             <Profile>
-                <Welcome>Ola, </Welcome>
+                <Welcome>Ola, {emoji}</Welcome>
                 <UserName>Adelto Oliveira</UserName>
             </Profile>
         </Container>
